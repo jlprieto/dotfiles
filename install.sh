87aa655836b0c94067f55d15c636e17c180104a7
jlprieto <3220204+jlprieto@users.noreply.github.com>
2019-03-01 14:42:20 -0800

    Adds Homebrew installation and virtual environment

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 VERSION="2019_01"
-LOG_FILE=~/.dotfile_log
+LOG_FILE=$HOME/.dotfile_log
 
 ###
 # msg [args]
@@ -80,7 +80,7 @@ testReturnValue () {
 
 writeToBashProfile(){
 		local str="$1"
-		echo "$str" >> ~/.bash_profile
+		echo "$str" >> $HOME/.bash_profile
 }
 
 ############
@@ -94,6 +94,7 @@ log "I'm going to start setting up this machine."
 msg "Let's start by backing the original .bash_profile and .bash_rc"
 backupFile ~/.bash_profile 
 backupFile ~/.bashrc 
+writeToBashProfile "source $HOME/.bashrc"
 
 ####
 # Check if I'm in a MacOS or in a Linux
@@ -107,7 +108,7 @@ msg "Looks like we're setting up a $OS_NAME"
 
 ####
 # Check that we actually cloned .dotfiles 
-if [ ! -d $HOME/.dotfiles ]; then
+if [[ ! -d $HOME/.dotfiles ]]; then
 		msg "There is NO .dotfiles folder. Clone .dotfiles from https://github.com/jlprieto/dotfiles.git"
 		exit 1
 else
@@ -137,6 +138,27 @@ else
 fi
 
 #####
+# Specific things for MACOS
+if [[ "$OS_NAME" == "OSX" ]]; then
+	msg "XCode"
+	msg "If XCode is already installed there will be an error message that can be savely ignored"
+	xcode-select --install 
+	confirm
+	testReturnValue "XCode"
+
+	if [[ -x /usr/local/bin/brew ]]; then
+		msg "Homebrew already installed, updating"
+		brew update
+	else
+		msg "Installing Homebrew"
+		confirm
+		ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" &&
+		source $HOME/.bash_profile
+		testReturnValue "Homebrew"
+	fi
+fi
+
+#####
 # Setup python stuff
 if [[ -n "$VIRTUAL_ENV" ]]; then
 	msg "You're on a virtual environment. Make sure you deactivate it before continuing"
@@ -152,16 +174,48 @@ if [ "$OS_NAME" == "LINUX" ]; then
 fi
 testReturnValue "pip3"
 
+if [[ "$OS_NAME" == "OSX" ]]; then
+	msg "Homebrew Python3"
+	brewInstallOrUpgrade python
+	testReturnValue "Homebrew Python3"
+
+	msg "Homebrew Python2"
+	brewInstallOrUpgrade python@2
+	testReturnValue "Homebrew Python2"
+
+	msg "gcc"
+	brewInstallOrUpgrade gcc
+	testReturnValue "gcc"
+
+	msg "cmake"
+	brewInstallOrUpgrade cmake
+	testReturnValue "cmake"
+
+	#needed for matplotlib
+	msg "freetype"
+	brewInstallOrUpgrade freetype
+	testReturnValue "freetype"
+
+	msg "hdf5"
+	brewInstallOrUpgrade hdf5
+	testReturnValue "hdf5"
+fi	
+
 msg "virtualenvwrapper"
 pip3 install virtualenvwrapper
 testReturnValue "virtualenvwrapper"
 
 msg "path_to_virtualenvwrapper"
+writeToBashProfile "export WORKON_HOME=$HOME/.venvs" 
 if [ "$OS_NAME" == "LINUX" ]; then
-	writeToBashProfile "export WORKON_HOME=$HOME/.venvs" 
 	writeToBashProfile "export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3" 
 	writeToBashProfile "export VIRTUALENVWRAPPER_VIRTUALENV=/usr/bin/virtualenv"
 	writeToBashProfile "source $HOME/.local/bin/virtualenvwrapper.sh"
+fi
+if [[ "$OS_NAME" == "OSX" ]]; then
+	writeToBashProfile "export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python3"
+	writeToBashProfile "export VIRTUALENVWRAPPER_VIRTUALENV=/usr/bin/virtualenv"
+	writeToBashProfile "source /usr/local/bin/virtualenvwrapper.sh"
 fi
 source $HOME/.bash_profile
 testReturnValue "path_to_virtualenvwrapper"
@@ -169,7 +223,10 @@ testReturnValue "path_to_virtualenvwrapper"
 msg "py3_virtualenv"
 if [ "$OS_NAME" == "LINUX" ]; then
 	mkvirtualenv --python=/usr/bin/python3 py3
-	writeToBashProfile "workon py3"
 fi
+if [[ "OS_NAME" == "OSX" ]]; then
+	mkvirtualenv --python=/usr/local/bin/python py3
+fi
+writeToBashProfile "workon py3"
 source $HOME/.bash_profile
 testReturnValue "py3_virtualenv"
